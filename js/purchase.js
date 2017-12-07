e482a01d44c51c799cbd0ed888bb91331a2d9953
ShadowOfYharnam <shadow.hunter_96@yahoo.com>
2017-12-07 06:32:29 -0800

    hopefully this works

--- js/purchase.js
+++ js/purchase.js
@@ -3,15 +3,22 @@ var totalPrice = 0;
 function addToOrder() {
     var selected = document.getElementById("selectBox1").value;
     var $p = $("<p></p>"); 
-    var price = 0;
+    var price = "";
     var fileName = "MOCK_DATA.json";
     //adds the price and name to a div below the button
 	$.getJSON(fileName, function(data){ 
 		$.each(data, function(i, e){
 			if(e.Name == selected){
                 price = e.price; 
-                totalPrice = totalPrice + price;
+                var priceDBL = Number(price.replace(/[^0-9\.-]+/g,""));
+                totalPrice = totalPrice + priceDBL;
+                
+                
                 $p.text(selected + " " + price + totalPrice);
+            
+        
+            
+            
             }
 		});		
 	});

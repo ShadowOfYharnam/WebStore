4cc39eef54fe434bea04dc26857a86ec45181c32
ShadowOfYharnam <shadow.hunter_96@yahoo.com>
2017-12-08 15:04:14 -0800

    rbh bgv

--- js/purchase.js
+++ js/purchase.js
@@ -1,11 +1,11 @@
 //contains the functions for the forms
 var totalPrice = 0;
+var selected = document.getElementById("selectBox1").value;
+var price = 0;
+        
 function addToOrder() {
-    var selected = document.getElementById("selectBox1").value;
     var $p = $("<p></p>");
     var $totalPrice = $("<p></p>"); 
-    var price = 0;
-    var fileName = "MOCK_DATA.json";
     //adds the price and name to a div below the button s well as total price math
     var data = $.getJSON(fileName);
     getPrice(data);
